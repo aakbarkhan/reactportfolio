d6a75bee4c96bbd91212b991255178ca08bd3731
aakbarkhan <aakuukhan@gmail.com>
2022-08-21 23:09:05 +0530

    add all the social icons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,9 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -12,15 +15,15 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Appontment Scheduler",
-      description: "Made appointment with Dr. Ruby, Ruby on Rails, PostgreSQL, JavaScript",
+      title: "Booking an Appointment.",
+      description: "Making an appointment with Doctor. Used  Ruby, Ruby on Rails, PostgreSQL, Bootstrap and JavaScript.",
       imgUrl: projImg1,
       live: "",
       github: "https://github.com/aakbarkhan/Appointment-app-backend",
     },
     {
       title: "metrics-webapp",
-      description: "This app shows the covid infection cases for different countries. Used React, Redux, Html&Css, JavaScript",
+      description: "This app shows the covid infection cases in different countries and can search with country names. Used React, Redux, Html & CSS, and JavaScript.",
       imgUrl: projImg2,
       live: 'https://coviid.netlify.app/',
       github: 'https://github.com/aakbarkhan/metrics-webapp',
@@ -33,19 +36,25 @@ export const Projects = () => {
       github: 'https://github.com/aakbarkhan/budget-app'
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Sea Foods App",
+      description: "This app is about adding likes and comments to sea foods. Used React, HTML&CSS and  JavaScript.",
+      imgUrl: projImg4,
+      live: 'https://aakbarkhan.github.io/Cfood/dist/',
+      github: 'https://github.com/aakbarkhan/Cfood'
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "TECHNOLOGY SUMMIT",
+      description: "Hello! Sharing world CREATIVE GLOBAL TECHNOLOGY SUMMIT 2021. Capstone poroject for HTML & CSS and  JavaScript",
+      imgUrl: projImg5,
+      live: 'https://aakbarkhan.github.io/capstone-project/index.html',
+      github: 'https://github.com/aakbarkhan/capstone-project'
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "Bookstore",
+      description: "It is about adding books with their category and used api created using Ruby on Rails.",
+      imgUrl: projImg6,
+      live: 'https://angry-bassi-da3bfc.netlify.app/',
+      github: 'https://github.com/aakbarkhan/bookstore'
     },
   ];
 
